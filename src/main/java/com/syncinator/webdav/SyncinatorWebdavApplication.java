ec6b599cea17337cf3b7abd6498ec74787735673
carlos <carlos@carlos>
2016-10-02 22:16:34 -0400

    temp

--- src/main/java/com/syncinator/webdav/SyncinatorWebdavApplication.java
+++ src/main/java/com/syncinator/webdav/SyncinatorWebdavApplication.java
@@ -1,6 +1,6 @@
  package com.syncinator.webdav;
 
-import java.util.Arrays;
+import java.io.File;
 
 import org.apache.jackrabbit.webdav.simple.SimpleWebdavServlet;
 import org.springframework.boot.SpringApplication;
@@ -8,9 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
-import com.onedrive.api.OneDrive;
-import com.onedrive.api.support.ClientCredential;
-import com.onedrive.api.support.Scope;
 import com.syncinator.webdav.cloud.onedrive.IdSerializatorAccessTokenListener;
 import com.syncinator.webdav.cloud.onedrive.OneDriveConnectionRepository;
 import com.syncinator.webdav.server.SyncinatorWebdavServlet;
@@ -27,13 +24,17 @@ public class SyncinatorWebdavApplication {
 		String prefix = "/webdav";
 		ServletRegistrationBean registration = new ServletRegistrationBean(new SyncinatorWebdavServlet(), prefix + "/*");
 		registration.addInitParameter(SimpleWebdavServlet.INIT_PARAM_RESOURCE_PATH_PREFIX, prefix);
-		
-		OneDrive oneDrive = new OneDrive(new ClientCredential("0000000048145120"),
-				Arrays.asList(Scope.OFFLINE_ACCESS, "wl.skydrive", "wl.signin", "onedrive.readwrite"),
-				OneDrive.MOBILE_REDIRECT_URI);
-		oneDrive.setAuthorizationCode("M62b3f60d-d2af-1aef-2716-d7be4ba4d2e3");
-		oneDrive.setAccessTokenListener(new IdSerializatorAccessTokenListener("1"));
-		OneDriveConnectionRepository.addConnection("1", oneDrive);
+
+		//temporal
+		File dir = new File(new IdSerializatorAccessTokenListener(null).getApplicationFolder());
+		if (dir.exists() && dir.isDirectory()){
+			for (File file : dir.listFiles()){
+				if (file.getName().endsWith(IdSerializatorAccessTokenListener.ACCESS_TOKEN_EXTENSION)){
+					String id = file.getName().substring(0, file.getName().length() - IdSerializatorAccessTokenListener.ACCESS_TOKEN_EXTENSION.length());
+					OneDriveConnectionRepository.addConnectionFromDisk(id);
+				}
+			}
+		}
 		
 	    return registration;
 	}
